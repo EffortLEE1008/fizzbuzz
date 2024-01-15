47aa0c86ce0a3817db4e44b949a5e81ac5ea0d05
gyoungtae-min <p.amin2719@gmail.com>
2024-01-15 17:56:49 +0900

    refactor : Create function do_fizzbuzz()

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,11 @@
-for i in range(1,21):
-	if i % 15 == 0:
-		print('fizzbuzz')
-	else:
-		print(i)
+def do_fizzbuzz():
+
+	for i in range(1,21):
+		if i % 15 == 0:
+			print('fizzbuzz')
+		else:
+			print(i)
+
+
+if __name__ == '__main__':
+	print(do_fizzbuzz())
